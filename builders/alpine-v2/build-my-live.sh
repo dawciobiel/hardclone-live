6d5bfe05fc51d83525ca5c710dccf1fcaa4e0bfe
Dawid Bielecki <dawciobiel@gmail.com>
2025-08-10 22:39:03 +0200

    Update build-my-live.sh

--- builders/alpine-v2/build-my-live.sh
+++ builders/alpine-v2/build-my-live.sh
@@ -1,49 +1,48 @@
 #!/bin/bash
 set -e
 
-ALPINE_VERSION="v3.20"
-ARCH="x86_64"
-OUTDIR="artifacts/alpine"
-APP_FILE="app.py"
-PROFILE_NAME="my-python-live"
+# Ustawienia
+PROFILE_NAME="custom-alpine"
+ARTIFACTS_DIR="/workspace/artifacts/alpine"
 
-mkdir -p "$OUTDIR"
+# Tworzenie katalogów
+mkdir -p "$ARTIFACTS_DIR"
+mkdir -p "./iso-root/root"
 
-# Download mkimage.sh
-if [ ! -f mkimage.sh ]; then
-    wget https://gitlab.alpinelinux.org/alpine/mkimage/-/raw/master/mkimage.sh
-    chmod +x mkimage.sh
-fi
+# Kopiowanie aplikacji
+cp /workspace/builders/alpine-v2/app.py ./iso-root/root/
 
-# Custom profile
-rm -rf ./profiles/$PROFILE_NAME
-mkdir -p ./profiles/$PROFILE_NAME
+# Instalacja minimalnego Alpine w katalogu iso-root
+apk --root ./iso-root --initdb add alpine-base python3 py3-pip
 
-cat > ./profiles/$PROFILE_NAME/packages <<EOF
-alpine-base
-python3
-py3-pip
-bash
+# Dodanie GRUB i ustawień bootowania
+mkdir -p ./iso-root/boot/grub
+cat > ./iso-root/boot/grub/grub.cfg <<EOF
+set default=0
+set timeout=5
+
+menuentry "Custom Alpine Live" {
+    linux /boot/vmlinuz root=/dev/ram0 alpine_dev=LABEL=ALPINE_LIVE modules=loop,squashfs,sd-mod,usb-storage quiet
+    initrd /boot/initramfs
+}
 EOF
 
-mkdir -p ./profiles/$PROFILE_NAME/airootfs/root
-cp /workspace/app.py ./profiles/$PROFILE_NAME/airootfs/root/
+# Pobranie jądra Alpine
+wget -O ./iso-root/boot/vmlinuz https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86_64/netboot/vmlinuz-lts
+wget -O ./iso-root/boot/initramfs https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86_64/netboot/initramfs-lts
 
-mkdir -p ./profiles/$PROFILE_NAME/airootfs/etc/local.d
-cat > ./profiles/$PROFILE_NAME/airootfs/etc/local.d/app.start <<'EOF'
-#!/bin/sh
-python3 /root/app.py
-EOF
-chmod +x ./profiles/$PROFILE_NAME/airootfs/etc/local.d/app.start
+# Utworzenie pliku ISO
+xorriso -as mkisofs \
+  -iso-level 3 \
+  -full-iso9660-filenames \
+  -volid "ALPINE_LIVE" \
+  -eltorito-boot boot/grub/i386-pc/eltorito.img \
+  -no-emul-boot -boot-load-size 4 -boot-info-table \
+  -eltorito-alt-boot \
+  -e boot/grub/efi.img \
+  -no-emul-boot \
+  -isohybrid-gpt-basdat \
+  -output "$ARTIFACTS_DIR/${PROFILE_NAME}.iso" \
+  ./iso-root
 
-mkdir -p ./profiles/$PROFILE_NAME/airootfs/etc/runlevels/default
-ln -sf /etc/init.d/local ./profiles/$PROFILE_NAME/airootfs/etc/runlevels/default/local
-
-# Build ISO
-./mkimage.sh \
-    --tag "$ALPINE_VERSION" \
-    --arch "$ARCH" \
-    --repository "http://dl-cdn.alpinelinux.org/alpine/$ALPINE_VERSION/main" \
-    --repository "http://dl-cdn.alpinelinux.org/alpine/$ALPINE_VERSION/community" \
-    --outdir "$OUTDIR" \
-    --profile "$PROFILE_NAME"
+echo "ISO gotowe: $ARTIFACTS_DIR/${PROFILE_NAME}.iso"
